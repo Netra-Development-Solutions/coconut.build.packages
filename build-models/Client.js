97120b4d3f5a24080bdb06e5d42e9d8c33742474
Naman Khater <56634706+MadMaxINDIAN@users.noreply.github.com>
2025-01-07 10:13:38 +0530

    client model updated

--- build-models/Client.js
+++ build-models/Client.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
+const constants = require("@coconut-packages/build-constants");
 
 const clientSchema = new mongoose.Schema({
     name: { type: String, required: true },
     clientCode: { type: String, required: true, unique: true, index: true, dropDups: true },
     clientId: { type: String, unique: true, index: true, dropDups: true },
-    modules: [{ type: mongoose.Schema.Types.ObjectId, ref: "Module" }],
+
     icon: { type: String, required: true },
-    }, { timestamps: true });
+    enabledEnvs: [{
+        env: { type: String, required: true, enum: constants.env },
+        modules: [{
+            module: { type: mongoose.Schema.Types.ObjectId, ref: "Module" },
+            application: { type: mongoose.Schema.Types.ObjectId, ref: "Application" }
+        }]
+    }],
+}, { timestamps: true });
 
 module.exports = mongoose.model("Client", clientSchema);
